354644db3469546f4d8c1cf17a82477c821407aa
CedricDC17 <cedric.dc91@gmail.com>
2023-12-21 16:03:09 +0100

    app.js/ mongoose / models / thing.js / server.js

---
+++ backend/app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const Thing = require('./models/thing');
+
+const app = express();
+
+// Middleware  CORS , parsing JSON
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.use(express.json());
+
+// Thing test
+const thing = new Thing({
+    title: 'Mon premier objet',
+    description: 'Les infos de mon premier objet',
+    imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+    userId: 'qsomihvqios',
+    review: 'Très bon livre.',
+});
+
+// Save Thing
+thing.save()
+    .then(thing => {
+        console.log('Objet enregistré avec succès !', thing);
+    })
+    .catch(err => {
+        console.error('Erreur lors de l\'enregistrement de l\'objet :', err);
+    });
+
+// API 
+app.post('/api/stuff', (req, res, next) => {
+    delete req.body._id;
+    const thing = new Thing({
+        ...req.body
+    });
+    thing.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+        .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/stuff/:id', (req, res, next) => {
+    Thing.findOne({ _id: req.params.id })
+        .then(thing => res.status(200).json(thing))
+        .catch(error => res.status(404).json({ error }));
+});
+
+// Export app
+module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,64 @@
 const http = require('http');
+const mongoose = require('mongoose');
+const app = require('./app');
 
-const server = http.createServer((req, res) => {
-    res.end('Voilà la réponse du serveur !');
+// Connexion à MongoDB
+mongoose.connect('mongodb+srv://user1:user1@grimoire.bp122n4.mongodb.net/?retryWrites=true&w=majority');
+const db = mongoose.connection;
+
+// Gestion des erreurs de connexion à MongoDB
+db.on('error', console.error.bind(console, 'Erreur de connexion à MongoDB :'));
+db.once('open', () => {
+    console.log('Connexion réussie à MongoDB !');
 });
 
-server.listen(process.env.PORT || 3000);
+// Normalisation du port
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+};
+
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+// Gestionnaire d'erreurs liées au serveur
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' nécessite des privilèges élevés.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' est déjà utilisé.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+// Création du serveur
+const server = http.createServer(app);
+
+// Gestion des erreurs et affichage de l'adresse d'écoute
+server.on('error', errorHandler);
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Écoute sur ' + bind);
+});
+
+// Démarrage du serveur
+server.listen(port);
