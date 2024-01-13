6835838afa6787c3f17bc7d32d5bf515c07f861d
CedricDC17 <cedric.dc91@gmail.com>
2024-01-13 18:09:15 +0100

    Verification format e mail

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,9 +1,21 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
+function validEmail(email) {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    return emailRegex.test(email);
+}
 
 const userSchema = mongoose.Schema({
-    email: { type: String, required: true, unique: true },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        validate: {
+            validator: validEmail,
+            message: 'L\'email n\'est pas au bon format'
+        }
+    },
     password: { type: String, required: true }
 });
 
